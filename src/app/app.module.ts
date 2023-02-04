8ba9482fffcc20262e3501d650405f31a1e7ac84
Andrew-Gautier <111550745+Andrew-Gautier@users.noreply.github.com>
2023-02-04 08:11:00 -0500

    Lab 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NavbarComponent } from "./navigation/navbar/NavbarComponent";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
