95781632b1c7b658464f7a6f6c0521ddaa32fece
Andrew-Gautier <111550745+Andrew-Gautier@users.noreply.github.com>
2023-02-23 09:57:29 -0500

    Lab 2 Update
    Added Cards and a Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './header/navigation.component';
 import { NavbarComponent } from "./header/navbar/navbar.component";
+import { CardsComponent } from './header/cards/cards.component';
+import { FooterComponent } from './header/footer/footer.component';
+import { Cards2Component } from './header/cards2/cards2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    NavbarComponent
+    NavbarComponent,
+    CardsComponent,
+    FooterComponent,
+    Cards2Component
   ],
   imports: [
     BrowserModule,
