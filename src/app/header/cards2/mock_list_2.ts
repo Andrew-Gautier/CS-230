c59b28b97e581634444c37bf66cc220d46ce89a1
Andrew-Gautier <111550745+Andrew-Gautier@users.noreply.github.com>
2023-03-07 01:50:03 -0500

    Lab 3 Photos update

--- src/app/header/cards2/mock_list_2.ts
+++ src/app/header/cards2/mock_list_2.ts
@@ -4,14 +4,14 @@ export const mock_list_2 = [
 
         title: "“I have time for my family again”",
         description: "Before Jobber, I was doing all these admin-focused tasks after-hours when I could have been spending time with my family.",
-        img: "assets/home-coaching.jpeg",
+        img: "assets/Testimonial Headshots/Testimonial-headshot-1.png",
         imgDescription: "not found"
 
     }, //card1 desc 
     {
         title: "“We stay in our own lanes now”",
         description: "Jobber has led us to stay in our lanes because if you follow the Jobber workflow, it keeps you on track with your responsibilities.",
-        img: "assets/home-community.jpeg",
+        img: "assets/Testimonial Headshots/testimonial-thumb-christine.png",
         imgDescription: "not found",
 
 
@@ -19,7 +19,7 @@ export const mock_list_2 = [
     {
         title: "“Jobber has saved our lives”",
         description: "Jobber has saved our lives. There’s so much coordinating between the two trucks, it’s impossible to scale our company without a booking software.",
-        img: "assets/home-app-1.jpeg",
+        img: "assets/Testimonial Headshots/testimonial-thumb-julia.png",
         imgDescription: "not found",
 
 
